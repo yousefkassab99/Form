ceff0aee64b81ca83b66678689ef711ee2cd2d09
yousef kassab <yousefkassabw.99@gmail.com>
2023-01-05 11:38:30 +0300

    ch

--- src/componants/leftbar/Leftbar.jsx
+++ src/componants/leftbar/Leftbar.jsx
@@ -9,22 +9,57 @@
   import su from "../../assets/icons/su.webp"
   export default function Leftbar() {
     return (
-      <div className="leftbar">
-        <div className="leftbarWrapper">
+
+      <>
+      <div className="bar">
+          <div className="logo">
+              <ul>
+                  <li><img src={g} alt="logo-game" className="img" /></li>
+              </ul>
+          </div>
+          <div className="menu">
+              <ul>
+                  <li>
+                      <img src={h} alt="like" />
+                  </li>
+                  <li>
+                      <img src={ss} alt="setting" />
+                  </li>
+                  <li>
+                      <img src={p} alt="puzzle" />
+                  </li>
+              </ul>
+          </div>
+          <div className="theme">
+              <ul>
+                  <li>
+                      <img src={m} alt="moon" />
+                  </li>
+                  <li>
+                      <img src={su} alt="sun" className="sun" width="100px" />
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </>
+
+
+      // <div className="leftbar">
+      //   <div className="leftbarWrapper">
        
           
-        <span  className="g"> <img src={g} width="50px"/></span>
-        <div className="second-div">
-          <span className="m"><img src={h}width="30px" /></span> 
-          <span className="m"><img src={ss}width="30px"/></span> 
-          <span className="m"><img src={p} width="30px"/></span> 
-          </div>
-          <div className="third-div">
-          <span className="st1"><img src={m}width="30px" /></span>
-          <span className="st2"><img src={su}width="30px" /></span> 
-          </div>
-        </div>
-      </div>
+      //   <span  className="g"> <img src={g} width="50px"/></span>
+      //   <div className="second-div">
+      //     <span className="m"><img src={h}width="30px" /></span> 
+      //     <span className="m"><img src={ss}width="30px"/></span> 
+      //     <span className="m"><img src={p} width="30px"/></span> 
+      //     </div>
+      //     <div className="third-div">
+      //     <span className="st1"><img src={m}width="30px" /></span>
+      //     <span className="st2"><img src={su}width="30px" /></span> 
+      //     </div>
+      //   </div>
+      // </div>
     );
   }
   
